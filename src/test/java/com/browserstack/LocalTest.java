dee2fba91624ff0bd9fee274dcdbbd1cae2f44e9
surbhi-maker <mamarsurbhi99@gmail.com>
2024-10-02 01:49:24 +0530

    git project Oct 2024

---
+++ src/test/java/com/browserstack/LocalTest.java
@@ -0,0 +1,9 @@
+package com.browserstack;
+
+
+public class LocalTest{
+
+	public static void main(String args[]) {
+		System.out.println("Git Class 1");
+	}
+}
---
+++ src/test/java/com/browserstack/LocalTestDemo.java
@@ -0,0 +1,9 @@
+package com.browserstack;
+
+
+public class LocalTestDemo{
+
+	public static void main(String args[]) {
+		System.out.println("Git Class 2");
+	}
+}
